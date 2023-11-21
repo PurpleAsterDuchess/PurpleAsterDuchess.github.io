abc6a56d97b24e7416ae42e51aaebcf73484296f
PurpleAsterDuchess <prinaparekh9@gmail.com>
2023-11-21 09:56:25 +0000

    working but creating a new tab with problems

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const save = (event) => {
-    event.preventDefault();
-    let blogEntry = document.getElementById("entry").value;
-    localStorage.setItem("entry", blogEntry);
-    const p = document.getElementById("output1");
-    const func1 = () => p.innerHTML = localStorage.getItem("entry");
-    done.addEventListener('click', func1)
-};
+// const save = (event) => {
+//     event.preventDefault();
+//     let blogEntry = document.getElementById("entry").value;
+//     localStorage.setItem("entry", blogEntry);
+//     const p = document.getElementById("output1");
+//     const func1 = () => p.innerHTML = localStorage.getItem("entry");
+//     done.addEventListener('click', func1)
+// };
 
 function countText () {
     let text = document.getElementById("entry").value;
     document.getElementById('current').innerText = text.length;
+}
+
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("entry").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("output1").appendChild(li);
+    }
+    document.getElementById("entry").value = "";
+
+    countText();
 }
