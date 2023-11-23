e9fdd9e0b538e9ecc04527a3a325863575bd833b
PurpleAsterDuchess <prinaparekh9@gmail.com>
2023-11-23 10:15:48 +0000

    mobile compatable

--- index.js
+++ index.js
@@ -65,9 +65,13 @@ const navbar = () => {
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("sidenav").style.width = "640px";
-    document.getElementById("main").style.marginLeft = "640px";
-    document.getElementById("main").style.opacity = "0.5";
+    if (window.innerWidth > 721) {
+        document.getElementById("sidenav").style.width = "10%";
+        document.getElementById("main").style.marginLeft = "10%";
+        document.getElementById("main").style.opacity = "0.5";
+    } else {
+        document.getElementById("sidenav").style.width = "100%";
+    }
 }
 
 /* Set the width of the side navigation to 0 */
