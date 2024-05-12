c61892582d6386a0c509604ecb4d72e6b13675dc
prina <prinaparekh9@gmail.com>
2024-05-12 14:55:54 +0100

    #set up the file

---
+++ src/kaboomCtx.js
@@ -0,0 +1,9 @@
+import kaboom from "kaboom";
+
+export const k = kaboom({
+    global: false,
+    // makes it so you can write tap events for mobile the same as mouse events so it is intigratable
+    touchToMouse: true,
+    canvas: document.getElementById("game"),
+
+})
