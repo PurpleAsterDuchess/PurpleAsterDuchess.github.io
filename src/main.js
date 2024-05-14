d05563c7b5cb196df12a2e5533c0436ead40b100
prina <prinaparekh9@gmail.com>
2024-05-14 23:30:34 +0100

    a lot of stuff...

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { SPRITE, scaleFactor } from "./constant";
+import { SPRITE, dialogueData, scaleFactor } from "./constant";
 import { k } from "./kaboomCtx";
+import displayDialogue, { setCamScale } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     // number of frames
@@ -8,11 +9,11 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceY: 31,
     anims: {
         "idle-down": SPRITE,
-        "walk-down": { from: SPRITE, to: (SPRITE + 4), loop: true, speed: 8 },
+        "walk-down": { from: SPRITE, to: (SPRITE + 3), loop: true, speed: 8 },
         "idle-side": (SPRITE + 39),
-        "walk-side": { from: (SPRITE + 39), to: (SPRITE + 39 + 4), loop: true, speed: 8 },
+        "walk-side": { from: (SPRITE + 39), to: (SPRITE + 39 + 3), loop: true, speed: 8 },
         "idle-up": (SPRITE + 39 + 39),
-        "walk-up": { from: (SPRITE + 39 + 39), to: (SPRITE + 39 + 39 + 4), loop: true, speed: 8 },
+        "walk-up": { from: (SPRITE + 39 + 39), to: (SPRITE + 39 + 39 + 3), loop: true, speed: 8 },
     },
 })
 
@@ -29,6 +30,8 @@ k.scene("main", async() => {
         k.pos(0),
         k.scale(scaleFactor)
     ]);
+
+    k.add(map);
 
     const player = k.make([
 
@@ -57,7 +60,7 @@ k.scene("main", async() => {
     ]);
 
     for (const layer of layers) {
-        if (layer.name == "bounds") {
+        if (layer.name === "bounds") {
             for (const bounds of layer.objects) {
                 map.add([
                     k.area({
@@ -71,12 +74,93 @@ k.scene("main", async() => {
                 if (bounds.name) {
                     player.onCollide(bounds.name, () => {
                         player.isInDialogue = true;
-                        // TODO
+                        displayDialogue("To Do", () => (player.isInDialogue = false))
                     })
                 }
             }
         }
+
+        if (layer.name === "spawnpoint") {
+            for (const entity of layer.objects) {
+                if (entity.name === "player") {
+                    player.pos = k.vec2(
+                        (map.pos.x + entity.x) * scaleFactor,
+                        (map.pos.y + entity.y) * scaleFactor
+                    );
+                    k.add(player);
+                }
+            }
+        }
     }
+
+    setCamScale(k)
+
+    k.onResize(() => {setCamScale(k)})
+
+    k.onUpdate(() => {
+        k.camPos(player.pos.x, player.pos.y + 100);
+    })
+
+    k.onMouseDown((mouseBtn) => {
+        if (mouseBtn != "left" || player.isInDialogue) return;
+        const worldMousePos = k.toWorld(k.mousePos());
+        player.moveTo(worldMousePos, player.speed);
+
+        const mouseAngle = player.pos.angle(worldMousePos);
+
+        const lowerBound = 50;
+        const upperBound = 125;
+
+        if (
+            mouseAngle > lowerBound &&
+            mouseAngle < upperBound &&
+            player.curAnim() !== "walk-up"
+        ) {
+            player.play("walk-up");
+            player.direction = "up";
+            return;
+        }
+
+        if (
+            mouseAngle < -lowerBound &&
+            mouseAngle > -upperBound &&
+            player.curAnim() !== "walk-down"
+        ) {
+            player.play("walk-down");
+            player.direction = "down";
+            return;
+        }
+
+        if (Math.abs(mouseAngle) > upperBound) {
+            player.flipX = false;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "right";
+            return;
+        }
+
+        if (Math.abs(mouseAngle) < lowerBound) {
+            player.flipX = true;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "left";
+            return;
+        }
+
+    });
+
+    k.onMouseRelease(() => {
+        if (player.direction === "down") {
+            player.play("idle-down");
+            return;
+        };
+        if (player.direction === "up") {
+            player.play("idle-up");
+            return;
+        };
+        player.play("idle-side");
+    })
+
 });
+
+
 
 k.go("main");
