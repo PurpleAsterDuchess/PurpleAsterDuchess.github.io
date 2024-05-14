89bbd6f334269a4fe408ee03a85d2252ab46f191
prina <prinaparekh9@gmail.com>
2024-05-14 18:33:44 +0100

    make the sprite and map

---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const SPRITE = 960
+export const scaleFactor = 4;
--- src/main.js
+++ src/main.js
@@ -1,24 +1,60 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { SPRITE, scaleFactor } from "./constant";
+import { k } from "./kaboomCtx";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+k.loadSprite("spritesheet", "./spritesheet.png", {
+    // number of frames
+    // each frame is 16*16 tile
+    sliceX: 39,
+    sliceY: 31,
+    anims: {
+        "idle-down": SPRITE,
+        "walk-down": { from: SPRITE, to: (SPRITE + 4), loop: true, speed: 8 },
+        "idle-side": (SPRITE + 39),
+        "walk-side": { from: (SPRITE + 39), to: (SPRITE + 39 + 4), loop: true, speed: 8 },
+        "idle-up": (SPRITE + 39 + 39),
+        "walk-up": { from: (SPRITE + 39 + 39), to: (SPRITE + 39 + 39 + 4), loop: true, speed: 8 },
+    },
+})
 
-setupCounter(document.querySelector('#counter'))
+k.loadSprite("map", "./02 messing with tiled/map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+k.scene("main", async() => {
+    const mapData = await (await fetch("./02 messing with tiled/map.json")).json();
+    const layers = mapData.layers;
+
+    const map = k.make([
+        k.sprite("map"),
+        k.pos(0),
+        k.scale(scaleFactor)
+    ]);
+
+    const player = k.make([
+
+        k.sprite("spritesheet", { anim: "idle-down" }),
+
+        // automatically create a hitbox for the player
+        k.area({
+            shape: 
+            new k.Rect(k.vec2(0, 3), 10, 10)
+        }),
+
+        k.body(),
+        k.anchor("center"),
+        k.pos(),
+        k.scale(scaleFactor),
+
+        {
+            speed: 250,
+            direction: "down",
+            isInDialogue: false,
+        },
+
+        // tag to check for collisions
+        "player",
+
+    ]);
+});
+
+k.go("main");
