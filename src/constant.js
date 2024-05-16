5b9db2d973ecc983dcaf5ba7720912a6eb8adb4c
prina <prinaparekh9@gmail.com>
2024-05-16 21:51:01 +0100

    adding a few more details

--- src/constant.js
+++ src/constant.js
@@ -33,15 +33,8 @@ export const dialogueData = {
     <li>English language: 5</li>
     <li>French: 5</li>
     </ul>`,
-    "A-Levels": `Ah, my A-levels. I worked very hard for those. I've always been interested in STEM so I took A-levels I found interesting.
-    <ul>
-    <li>Maths: A</li>
-    <li>Chemisty: B</li>
-    <li>Biology: B</li>
-    </ul>
-
-    I had a few other things I was working towards during my studies... you can walk around a bit and find out more about what they were`,
-    CV: ` You can learn about me in more detail here but if you're short on time, you could download my <a>CV</a>. It's not working yet but it will soon...`,
+    "A-Levels": `Ah, my A-levels. I worked very hard for those. I've always been interested in STEM so I took A-levels I found interesting. You can find my grades in my CV. I had a few other things I was working towards during my studies... you can walk around a bit and find out more about what they were`,
+    CV: `A gamified version of my CV...Coming soon :)`,
     "Code snippets": `I've done some bits of code for projects as well. I'm still compiling these but you could always come back later and have a look :)`,
     Volunteering: `I've done quite a bit of volunteering. I enjoy helping people and inspiring them. I've done some volunteering in a Covid vaccination center. It was very rewarding and it meant I could talk to a variety of people and I was able to comfort them through the uncertainty of getting vaccinated. I also did volunteering in a charity that aimed to raise money for children that could not afford an education. I had to organise events and raise money with a team. It taught me a lot about time management as well as working with and managing a team along with many other skills. I am also a STEM ambassador. I enjoy learning new things and I hope to inspire others to feel the same.`,
     DofE: `I've done DofE and gotten a certificate [cert]....`,
@@ -49,5 +42,11 @@ export const dialogueData = {
     Guitar: `I began with playing the acoustic guitar and bought an electric guitar a few years into my journey. I prefer playing electric because most technical songs are on electric, though there are quite a few good songs on acoustic that are fun to play.`,
     Flute: `I started playing flute around 9 years old. It used to be my favorite instrument but I couldn't find many songs I wanted to learn on it after a while. I still enjoy playing it and would love to find more songs to spark my inspiration once again.`,
     Singing: `I love singing! The voice is such a portable instrument and it's so versitile. `,
+    Lambda: `I have done some Lambda Exams for poetry. For this, I had to memorise a poem and an extract from a play and act it out. I had a lot of fun with it and it taught me a lot about public speaking and body language when presenting.`,
+    Swimming: `I used to love swimming! I did some grades on this too. I don't swim much anymore but I'm always up for it when there's an opportunity.`,
+    Art: `Ooh my art! I love drawing and I've been getting into different forms of art recently to further my skills. You could check out my <a href=https://www.instagram.com/prina_9?igsh=c3VqeGJ1NzFoeGFl>art account</a> to see some of my favorites!`,
+    CREATE: `I took part in this tripple threat theater group. We would perform shows and I would act, sing and dance in them. I learnt many things from there, including tap dancing.`,
+    Hiking: `I love doing sports, one of my favorite things to do is Hiking. It's a lot of fun, especially when you get lost and have to find your way back to the trail.`,
+    Skiing: `Skiing is my favorite sport! I love being in the mountains and seeing the sights. I'm very comfortable skiing down quite a few slopes and I hope to start doing some tricks soon!`,
 
 }
